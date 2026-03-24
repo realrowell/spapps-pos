71758be6703adeada7bb382f244e9b181c3ae0ad
rowelljay <bioseed.real@gmail.com>
2026-03-24 23:13:42 +0800

    partial: product details page

--- resources/js/pages/inventories/ProductDetails.vue
+++ resources/js/pages/inventories/ProductDetails.vue
@@ -8,6 +8,16 @@ import { type BreadcrumbItem } from '@/types';
 import type { Product } from '@/types/inventories/product';
 import Label from '@/components/ui/label/Label.vue';
 import { Input } from '@/components/ui/input';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import formatToCurrency from '@/composables/point-of-sale/formatToCurrency';
 
 
 const { product } = defineProps<{
@@ -60,16 +70,32 @@ const form = useForm({
 
             </div>
             <div class="relative rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border p-5 bg-card">
-                <Label>Product Name:</Label>
-                <Input
-                    class="disabled:text-black "
-                    disabled
-                    v-model="form.name"/>
-                <span></span>
-                <Label>Product Code: </Label>
-                <span>{{ product.pr_code }}</span>
-                <Label>Product Description: </Label>
-                <span>{{ product.pr_desc }}</span>
+                <div class="flex flex-row gap-3">
+                    <div class="flex flex-col gap-2 flex-1">
+                        <img src="/images/default_product.jpg" alt="product image" class="fill w-full h-auto aspect-square border-4">
+                    </div>
+                    <div class="flex flex-col gap-2 flex-2">
+                        <h3 class="text-xl">{{ product.pr_name }}</h3>
+                        <p class="text-sm text-muted-foreground">{{ product.pr_short_desc }}</p>
+                        <div class="flex flex-row gap-3">
+                            <div v-for="price in product.prices" :key="price.price_code" class="flex flex-col gap-1">
+                                <Card class="w-48">
+                                    <CardContent>
+                                        <CardTitle>{{ price.price_type }}</CardTitle>
+                                        <CardDescription v-if="price.effective_from != null && price.effective_to != null">
+                                            Effective from {{ price.effective_from }} to {{ price.effective_to }}
+                                        </CardDescription>
+                                        <p class="text-lg font-semibold">{{ formatToCurrency(price.price) }}</p>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </div>
+                        <div class="flex flex-row gap-3">
+                            <Button variant="outline" size="sm">Edit</Button>
+                            <Button variant="outline" size="sm" color="destructive">Delete</Button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </AppLayout>
